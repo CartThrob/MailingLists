81a789f9a3c227fabb11cb7cb3174ff96ea16219
Shawn Maida <shawn.maida@fostermade.co>
2022-11-20 20:48:19 -0500

    Update for CT 7+

---
+++ system/user/addons/cartthrob_mailing_lists/src/Drivers/MailChimpDriver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CartThrob\MailingLists\Drivers;
+
+use CartThrob\MailingLists\Interfaces\MailingListInterface;
+
+class MailChimpDriver implements MailingListInterface
+{
+    public function __construct()
+    {
+    }
+
+    public function subscribe()
+    {
+        // TODO: Implement subscribe() method.
+    }
+
+    public function unsubscribe()
+    {
+        // TODO: Implement unsubscribe() method.
+    }
+}
--- system/user/addons/cartthrob_mailing_lists/src/Hooks/CpMenuArrayHook.php
+++ system/user/addons/cartthrob_mailing_lists/src/Hooks/CpMenuArrayHook.php
@@ -8,11 +8,13 @@ class CpMenuArrayHook extends Hook
 {
     public function process(array $menu = []): array
     {
+        $menu = array_shift($menu);
+
         if (ee()->extensions->last_call !== false) {
             $menu = ee()->extensions->last_call;
         }
 
-        $menu[0]['ct.route.nav.addons']['list']['ct.ml.nav'] = [
+        $menu['ct.route.nav.addons']['list']['ct.ml.nav'] = [
             'path' => 'addons/settings/cartthrob_mailing_lists',
             'with_base_url' => false,
         ];
